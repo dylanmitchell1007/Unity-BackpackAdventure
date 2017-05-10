71fa88417ae7aeca9431c3b8b1e3399b5e60c1ef
MaxGuidry <mguidry7@outlook.com>
2017-05-10 17:00:20 -0500

    Dylan did this and I screwed it up this is the fix

---
+++ Assets/Scripts/CombatKnife.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "CombatKnife", menuName = "Item/Equipment/Weapon/Combat Knife",order = 2)]
+    public class CombatKnife : Weapon
+    {
+
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScriptableObjects
+{
+    public class Item : ScriptableObject
+    {
+        public int id;
+        public string m_name;
+    }
+}
