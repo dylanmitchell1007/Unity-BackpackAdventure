c40f89a6f85de8152e55e02e00a546b82489ec4d
MaxGuidry <mguidry7@outlook.com>
2017-05-19 12:07:19 -0500

    Scene change and testing itemlog

--- Assets/Scripts/ItemLog.cs
+++ Assets/Scripts/ItemLog.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class ItemLog : MonoBehaviour
 {
     public UnityEngine.UI.Text text;
-
+    private float timer = 0f;
+    private string firstSequence = "";
     private void Start()
     {
         text.text = "";
+    }
+
+    void Update()
+    {
+        if (timer > 7)
+        {
+            text.text = text.text.TrimStart(firstSequence.ToCharArray()) ;
+            timer = 0f;
+        }
+        timer += Time.deltaTime;
     }
 
     void OnEnable()
@@ -23,5 +34,12 @@ public class ItemLog : MonoBehaviour
     public void OnItemAction(Item item)
     {
         text.text += "Item picked up: " + item.m_name + "\n";
+        for (int i = 0; i < text.text.Length; i++)
+        {
+            firstSequence += text.text[i];
+            if (firstSequence[i] == '\n')
+                break;
+        }
+        
     }
 }
