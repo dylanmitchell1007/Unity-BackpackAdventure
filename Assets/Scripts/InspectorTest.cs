f2a157ab2372d07410c73a83d387669f768c26af
dylanmitchell1007 <armadummy@yahoo.com>
2017-05-25 17:55:30 -0500

    5/25/2017

---
+++ Assets/Scripts/InspectorTest.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Constraints;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+
+[CustomEditor(typeof(TestInspector))]
+public class InspectorTest : Editor
+{
+    public Text call;
+    public override void OnInspectorGUI()
+    {
+        TestInspector mybackpack = (TestInspector)target;
+
+        
+        mybackpack.items = EditorGUILayout.IntField("Inventory", mybackpack.items);
+        EditorGUILayout.LabelField("Items", mybackpack.items.ToString());
+        EditorGUILayout.("YOU PRESSED A BUTTON", name);
+        EditorGUILayout.DropdownButton(GUIContent.none, FocusType.Keyboard);
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TestInspector.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestInspector", menuName = "Inventory/TEST",order = 4)]
+public class TestInspector : ScriptableObject
+{
+
+    public int items;
+
+    public int backpack
+    {
+        get { return items / 750; }
+    }
+}
+
+public class Inventory : Item
+{
+    
+}
+
